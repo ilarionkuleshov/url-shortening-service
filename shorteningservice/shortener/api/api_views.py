796093b3e10b3edb07022df41d0b66ff0ceaed44
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2022-01-16 20:54:57 +0200

    some corrections

--- shorteningservice/shortener/api/api_views.py
+++ shorteningservice/shortener/api/api_views.py
@@ -7,18 +7,18 @@ from ..models import URL, create_url_object
 class URLApiView(APIView):
 
 	def post(self, request):
-		url = create_url_object(request, request.data['initial_url'])
+		url = create_url_object(request, request.query_params['initial_url'])
 
 		return Response({'short_url': url.short_url})
 
 	def get(self, request):
-		url = URL.objects.get(short_url=request.data['short_url'])
+		url = URL.objects.get(short_url=request.query_params['short_url'])
 		details = url.get_statistics(request)
 
 		return Response(details)
 
 	def delete(self, request):
-		url = URL.objects.get(short_url=request.data['short_url'])
+		url = URL.objects.get(short_url=request.query_params['short_url'])
 		details = url.delete(request)
 
 		return Response(details)
